002ab2e7d89d8b42b8f6a983d269b59836df4891
agerber0902 <agerber0902@gmail.com>
2026-02-17 11:43:09 -0500

    initial expo project

---
+++ PartnerAffirmations/app/index.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const App = () => {
+    return <SafeAreaView><Text>Partner Affirmations</Text></SafeAreaView>
+};
+export default App;
