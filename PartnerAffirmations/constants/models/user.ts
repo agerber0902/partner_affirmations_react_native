c30c35b8bdb4852a73260028ad51c8f26662e979
agerber0902 <agerber0902@gmail.com>
2026-02-25 20:23:09 -0500

    display creator name with the affirmation

--- PartnerAffirmations/constants/models/user.ts
+++ PartnerAffirmations/constants/models/user.ts
@@ -2,4 +2,14 @@ export interface AffirmationUser {
     id: string | undefined;
     uid: string;
     name: string;
+    displayNameForPartner: string;
+}
+
+export const AffirmationUserMap = (data: any, id: string): AffirmationUser => {
+    return {
+        id: id,
+        uid: data.uid,
+        name: data.uid,
+        displayNameForPartner: data.displayNameForPartner
+    };
 }
--- PartnerAffirmations/helpers/user-helper.ts
+++ PartnerAffirmations/helpers/user-helper.ts
@@ -1,4 +1,4 @@
-import { AffirmationUser } from "@/constants/models/user";
+import { AffirmationUser, AffirmationUserMap } from "@/constants/models/user";
 import { addData } from "./firebase-helper";
 import { User } from "firebase/auth";
 import { collection, getDocs, limit, query, where } from "firebase/firestore";
@@ -12,18 +12,18 @@ export const addUser = async (user: User) => {
     id: undefined,
     uid: user.uid,
     name: user.displayName ?? "",
+    displayNameForPartner: user.displayName ?? "",
   });
 };
 
-export const getUser = async (id: string) => {
+export const getUser = async (id: string): Promise<AffirmationUser | undefined> => {
   const ref = collection(firestore, collectionName);
-
-  const userQuery = query(ref, where("creatorId", "==", id), limit(1));
+  const userQuery = query(ref, where("uid", "==", id), limit(1));
   const snapshot = await getDocs(userQuery);
 
   if(snapshot.empty){
     return undefined;
   }
 
-  return snapshot.docs[0]
+  return AffirmationUserMap(snapshot.docs[0].data(), snapshot.docs[0].data().id);
 };
