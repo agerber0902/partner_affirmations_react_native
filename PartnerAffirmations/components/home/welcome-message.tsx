a1e2e43bb4988eabc3b9bbc5cdaab29ee526fa42
agerber0902 <agerber0902@gmail.com>
2026-02-27 11:31:24 -0500

    add a paced animation to the welcome text to make it warm and calm

--- PartnerAffirmations/components/home/welcome-message.tsx
+++ PartnerAffirmations/components/home/welcome-message.tsx
@@ -2,30 +2,65 @@ import { welcomeStyles } from "@/constants/stylesheets/components/welcome-styles
 import { Theme } from "@/constants/theme";
 import { useAuth } from "@/providers/auth-provider";
 import { View, Text } from "react-native";
+import FadeInView from "../shared/fade-in-animated-view";
 
 const WelcomeMessage = () => {
-  const { displayName } = useAuth();
+  const { displayName, isAuthenticated } = useAuth();
 
-  const getWelcomeMessage = () : {welcomeMessage: string, welcomeSubMessage: string} => {
+  // Animation order goes view, then twice the amount of time as the preceeding animation
+  const viewAnimationDuration = 1000;
+  const welcomeHeaderAnimationDuration = viewAnimationDuration * 2;
+  const welcomeSubHeaderAnimationDuration = welcomeHeaderAnimationDuration * 2;
+
+  const getWelcomeMessage = (): {
+    welcomeMessage: string;
+    welcomeSubMessage: string;
+  } => {
     const currentHour = new Date().getHours();
 
     if (currentHour >= 5 && currentHour < 12) {
-      return {welcomeMessage: `Good morning, ${displayName}`, welcomeSubMessage: `Begin each day with gratitude and positivity`};
+      return {
+        welcomeMessage: `Good morning, ${displayName}`,
+        welcomeSubMessage: `Begin each day with gratitude and positivity`,
+      };
     } else if (currentHour >= 12 && currentHour < 17) {
-      return { welcomeMessage: `Good afternoon, ${displayName}`, welcomeSubMessage: `Continue your day with gratitude and positivity`};
+      return {
+        welcomeMessage: `Good afternoon, ${displayName}`,
+        welcomeSubMessage: `Continue your day with gratitude and positivity`,
+      };
     } else if (currentHour >= 17 && currentHour < 21) {
-      return {welcomeMessage: `Good evening, ${displayName}`, welcomeSubMessage: `Take a breath and reflect on the day that was`};
+      return {
+        welcomeMessage: `Good evening, ${displayName}`,
+        welcomeSubMessage: `Take a breath and reflect on the day that was`,
+      };
     } else {
-      return {welcomeMessage: `Hello, ${displayName}`, welcomeSubMessage: `Take a breath to be present in the moment`};
+      return {
+        welcomeMessage: `Hello, ${displayName}`,
+        welcomeSubMessage: `Take a breath to be present in the moment`,
+      };
     }
   };
 
   return (
     <>
-      <View>
-        <Text id="welcome-header" style={[welcomeStyles.welcomeText]}>{getWelcomeMessage().welcomeMessage}</Text>
-        <Text id="welcome-sub-header" style={welcomeStyles.welcomeSubText}>{getWelcomeMessage().welcomeSubMessage}</Text>
-      </View>
+      <FadeInView duration={viewAnimationDuration} visible={isAuthenticated}>
+        <FadeInView
+          duration={welcomeHeaderAnimationDuration}
+          visible={isAuthenticated}
+        >
+          <Text id="welcome-header" style={[welcomeStyles.welcomeText]}>
+            {getWelcomeMessage().welcomeMessage}
+          </Text>
+        </FadeInView>
+        <FadeInView
+          duration={welcomeSubHeaderAnimationDuration}
+          visible={isAuthenticated}
+        >
+          <Text id="welcome-sub-header" style={welcomeStyles.welcomeSubText}>
+            {getWelcomeMessage().welcomeSubMessage}
+          </Text>
+        </FadeInView>
+      </FadeInView>
     </>
   );
 };
